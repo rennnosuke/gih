e2beb7599b0995c6fedbc1c59efce8f664c46b87
RenKanai <turutekab@gmail.com>
2019-12-11 17:47:22 +0900

    modify github/client.go : Github Issue作成処理追加

--- src/github/client.go
+++ src/github/client.go
@@ -25,16 +25,31 @@ type Client struct {
 	Organization   string
 }
 
+type IssueCreateRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
 func (c *Client) SearchIssues(searchWords []string) *[]byte {
 	params := make(map[string]string)
 	params["q"] = strings.Join(searchWords, " ")
-	return c.request("GET", c.endpoint(), params, nil)
+	return c.request("GET", c.endpoint(), &params, nil)
 }
 
-func (c *Client) request(method string, endpoint string, params map[string]string, body []byte) *[]byte {
+func (c *Client) CreateIssue(title, description string) *[]byte {
+	req := IssueCreateRequest{title, description}
+	body, err := json.Marshal(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Falied to create JSON request body. %#v", req)
+		os.Exit(1)
+	}
+	return c.request("POST", c.endpoint(), nil, body)
+}
+
+func (c *Client) request(method string, endpoint string, params *map[string]string, body []byte) *[]byte {
 
 	url := endpoint
-	queryParams := joinParams(&params)
+	queryParams := joinParams(params)
 	if queryParams != "" {
 		url += "?" + queryParams
 	}
